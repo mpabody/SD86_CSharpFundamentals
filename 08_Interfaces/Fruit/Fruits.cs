473f1f957e5084813850f7bbc103e78ad8bf2436
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-05-06 13:05:22 -0400

    Setup apple challenge

--- 08_Interfaces/Fruit/Fruits.cs
+++ 08_Interfaces/Fruit/Fruits.cs
@@ -34,4 +34,63 @@ namespace _08_Interfaces.Fruit
         }
 
     }
+
+    public class Orange : IFruit
+    {
+        // Constructors
+        public Orange() { }
+        public Orange(bool isPeeled)
+        {
+            IsPeeled = isPeeled;
+        }
+        public string Name
+        {
+            get
+            {
+                return "Orange";
+            }
+        }
+        public bool IsPeeled { get; private set; }
+
+        // Use the same interface method but bodies can be different as long as the return type matches
+        public string Peel()
+        {
+            if (IsPeeled)
+            {
+                return "It's already peeled";
+            }
+            else
+            {
+                IsPeeled = true;
+                return "You peeled the orange";
+            }
+        }
+
+        // Classes that use interfaces can still have unique properties and methods.
+        public string Squeeze()
+        {
+            return "You squeeze the orange, and juice comes out";
+        }
+    }
+
+    public class Grape : IFruit
+    {
+        //public Grape() { }
+        public string Name
+        {
+            get
+            {
+                return "Grape";
+            }
+        }
+        // Hardsetting property as false
+        public bool IsPeeled { get; } = false;
+
+        public string Peel()
+        {
+            return "Who peels grapes?";
+        }
+    }
+
+    // Make an Apple class inheriting from IFruit challenge
 }
