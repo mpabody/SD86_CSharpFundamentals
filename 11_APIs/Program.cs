91e1f51446dfc36253b1febaf37426ca6a94e44a
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-05-24 14:35:41 -0400

    Add API's

--- 11_APIs/Program.cs
+++ 11_APIs/Program.cs
@@ -29,7 +29,46 @@ namespace _11_APIs
             foreach (string vehicleUrl in person.Vehicles)
             {
                 HttpResponseMessage vehicleResponse = httpClient.GetAsync(vehicleUrl).Result;
-                Console.WriteLine(vehicleResponse.Content.ReadAsStringAsync().Result);
+                //Console.WriteLine(vehicleResponse.Content.ReadAsStringAsync().Result);
+                Vehicle vehicle = vehicleResponse.Content.ReadAsAsync<Vehicle>().Result;
+                Console.WriteLine(vehicle.Name);
+            }
+
+            SWAPIService swapiService = new SWAPIService();
+
+            Person personTwo = swapiService.GetPersonAsync(5).Result;
+            if (personTwo != null)
+            {
+                Console.WriteLine(personTwo.Name);
+
+                foreach(string vehicleUrl in personTwo.Vehicles)
+                {
+                    Vehicle vehicle = swapiService.GetVehicleAsync(vehicleUrl).Result;
+                    Console.WriteLine(vehicle.Name);
+                }
+            }
+
+            Vehicle genericResponse = swapiService.GetAsync<Vehicle>("https://swapi.dev/api/vehicles/4").Result;
+            if (genericResponse != null)
+            {
+                Console.WriteLine(genericResponse.Name);
+            }
+            else
+                Console.WriteLine("No vehicle exists here");
+
+
+            SearchResult<Person> skywalkers = swapiService.GetPersonSearchAsync("Skywalker").Result;
+            Console.WriteLine(skywalkers.Count);
+            foreach(Person personResult in skywalkers.Results)
+            {
+                Console.WriteLine(personResult.Name);
+            }
+
+            SearchResult<Vehicle> starfighters = swapiService.GetVehicleSearchAsync("starfighter").Result;
+            Console.WriteLine(starfighters.Count);
+            foreach(Vehicle vehicleResult in starfighters.Results)
+            {
+                Console.WriteLine(vehicleResult.Name);
             }
 
             Console.ReadKey();
